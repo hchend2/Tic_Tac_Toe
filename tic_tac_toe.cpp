c4c30c9adc9591918b0df55fca9524ed4309318c
Herve Chendjou <hchend2@uic.edu>
2021-12-31 03:41:43 -0600

    label added

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -6,16 +6,17 @@ using std::cout;
 // draw the gameboard...
 void grid() {
 
-    cout << "       ________________________    " << endl;
-    cout << "       |      |       |       |    " << endl;
-    cout << "       |      |       |       |    " << endl;
-    cout << "       |______|_______|_______|    " << endl;
-    cout << "       |      |       |       |    " << endl;
-    cout << "       |      |       |       |    " << endl;
-    cout << "       |______|_______|_______|    " << endl;
-    cout << "       |      |       |       |    " << endl;
-    cout << "       |      |       |       |    " << endl;
-    cout << "       |______|_______|_______|    " << endl;
+    int board[9] = {1,2,3,4,5,6,7,8,9};
+    cout << "      "<<"________________________    " << endl;
+    cout << "      "<<"|      |       |       |    " << endl;
+    cout << "      "<<"|  "<<board[0]<<"   |   "<<board[1] << "   |   " << board[2] << "   |    " << endl;
+    cout << "      "<<"|______|_______|_______|    " << endl;
+    cout << "      "<<"|      |       |       |    " << endl;
+    cout << "      "<<"|  "<<board[3]<<"   |   "<<board[4] << "   |   " << board[5] << "   |   " << endl;
+    cout << "      "<<"|______|_______|_______|    " << endl;
+    cout << "      "<<"|      |       |       |    " << endl;
+    cout << "      "<<"|  "<<board[6]<<"   |   "<<board[7] << "   |   " << board[8] << "   |   " << endl;
+    cout << "      "<<"|______|_______|_______|    " << endl;
 }
 
 int main() {
