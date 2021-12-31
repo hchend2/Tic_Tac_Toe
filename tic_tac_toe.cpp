06bff2035876872520d7bcec8681293f8fe42255
Herve Chendjou <hchend2@uic.edu>
2021-12-31 03:05:38 -0600

    grid added

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 
-using namespace std;
+using std::endl;
+using std::cout;
+
+// draw the gameboard...
+void grid() {
+
+    cout << "       ________________________    " << endl;
+    cout << "       |      |       |       |    " << endl;
+    cout << "       |      |       |       |    " << endl;
+    cout << "       |______|_______|_______|    " << endl;
+    cout << "       |      |       |       |    " << endl;
+    cout << "       |      |       |       |    " << endl;
+    cout << "       |______|_______|_______|    " << endl;
+    cout << "       |      |       |       |    " << endl;
+    cout << "       |      |       |       |    " << endl;
+    cout << "       |______|_______|_______|    " << endl;
+}
 
 int main() {
     cout << "Welcome to my Tic Tac Toe game!" << endl;
+    cout << endl << " The board! " << endl;
+    grid();
     return 0;
 }
