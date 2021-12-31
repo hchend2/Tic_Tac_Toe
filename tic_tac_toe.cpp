b1c43262163deb5857d5e81b98adf9ef72373c8f
Herve Chendjou <hchend2@uic.edu>
2021-12-31 03:45:47 -0600

    cmt

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -9,7 +9,7 @@ void grid() {
     int board[9] = {1,2,3,4,5,6,7,8,9};
     cout << "      "<<"________________________    " << endl;
     cout << "      "<<"|      |       |       |    " << endl;
-    cout << "      "<<"|  "<<board[0]<<"   |   "<<board[1] << "   |   " << board[2] << "   |    " << endl;
+    cout << "      "<<"|  "<<board[0]<<"   |   "<<board[1] << "   |   " << board[2] << "   |   " << endl;
     cout << "      "<<"|______|_______|_______|    " << endl;
     cout << "      "<<"|      |       |       |    " << endl;
     cout << "      "<<"|  "<<board[3]<<"   |   "<<board[4] << "   |   " << board[5] << "   |   " << endl;
@@ -17,6 +17,10 @@ void grid() {
     cout << "      "<<"|      |       |       |    " << endl;
     cout << "      "<<"|  "<<board[6]<<"   |   "<<board[7] << "   |   " << board[8] << "   |   " << endl;
     cout << "      "<<"|______|_______|_______|    " << endl;
+}
+
+void playGame() {
+    
 }
 
 int main() {
